74c96c9ba2882ba82bff5cdf93940dfabc5fa958
Jesse Hoare Evans <jesse.hoareevans@gmail.com>
2017-07-07 09:35:54 -0700

    basic admin priviliges

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
    before_action :configure_permitted_parameters, if: :devise_controller?
+   before_action :authenticate_user!, except: [:home]
 
   def home
 
@@ -9,7 +10,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :admin])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :admin])
   end
 end
